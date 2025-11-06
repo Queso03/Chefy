4924e10808b0308d282f932255aa5d22c5649fa4
ignacio.tejera <ignacio.tejera@fing.edu.uy>
2025-11-05 21:55:57 -0300

    initialize chefy-app with Spring Boot and Maven wrapper

---
+++ src/main/java/com/chefy/chefyapp/ChefyAppApplication.java
@@ -0,0 +1,13 @@
+package com.chefy.chefyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chefy/chefyapp/ChefyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefy.chefyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
